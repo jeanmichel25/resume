ad7afc5af2a4902d4be4a52f1de49cc2b3137016
Jean-Michel Alturk <jean-michel.alturk@polymtl.ca>
2024-03-13 12:20:27 -0400

    removed unused variable

--- src/MotionComponents.tsx
+++ src/MotionComponents.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { motion, useInView, useAnimation } from "framer-motion";
 import { TypeAnimation } from 'react-type-animation';
 
