85748950b19fdbe30b593837b5870162eb2ad1b4
Jean-Michel Alturk <jean-michel.alturk@polymtl.ca>
2024-04-03 16:43:52 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ReactComponent as LinkedInSvg } from './assets/icons/linkedin.svg';
 import './App.css';
 import { TypeAnimation } from 'react-type-animation';
 import { DelayedReveal, VeryDelayedReveal, WriteText, Languages } from './MotionComponents.tsx';
-import { EducationComponent, WorkComponent, HackathonComponent, ProjectsComponent, ExtracurricularComponent } from './ResumeComponents.tsx';
+import { EducationComponent, WorkComponent, HackathonComponent, ProjectsComponent } from './ResumeComponents.tsx';
 import { LottieScrollDown } from './LottieComponents.tsx';
 
 function App() {
